502f4b5e49d7db3e215da5f80f8bd18ef7956166
Francis <frmindmaze@gmail.com>
2020-03-08 22:15:04 +0530

    Initial commit

---
+++ src/main/java/Runner.java
@@ -0,0 +1,7 @@
+import com.intuit.karate.junit4.Karate;
+import cucumber.api.CucumberOptions;
+import org.junit.runner.RunWith;
+@RunWith(Karate.class)
+@CucumberOptions(features = "src/main/resources",tags = ("@Test"))
+public class Runner {
+}
